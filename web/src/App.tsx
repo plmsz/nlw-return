223527f1a6bb187f3a9de175d4e523e2e87cf87b
paloma.souza <paloma.souza@ambevtech.com.br>
2022-05-19 14:48:17 -0300

    Stage 1 Personalizando o botão com tailwind Biblioteca headlessui - para acessibilidade

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,18 +1,8 @@
-interface ButtonProps {
-  text?: string;
-}
-
-function Button(props: ButtonProps) {
-  return (
-    <button className='bg-violet-500 px-4 h-10 rounded'>{props.text ?? 'Default'}</button>
-  );
-}
+import { Widget } from './component/Widget';
 
 function App() {
   return (
-    <div className='flex gap-2'>
-      <Button /> <Button text='Enviar' />
-    </div>
+    <Widget/>
   );
 }
 
